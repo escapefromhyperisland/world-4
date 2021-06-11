6f040a03b7ad10aa634e6adf26dc52edc137684e
Lovisa Elensky <70098425+lovisaelensky@users.noreply.github.com>
2021-06-11 09:42:21 +0200

    Fix background music

--- level-1/scripts/level1.js
+++ level-1/scripts/level1.js
@@ -4,9 +4,13 @@ const modalGuy = document.querySelector('.modal-conversation');
 const introModal = document.querySelector('.modal-intro');
 const closeBtn = document.querySelector('.close-btn');
 const closeBtnTwo = document.querySelector('.close-btn-two');
+const bgMusic = new Audio(
+  'assets/i-have-sinned-creepy-mysterious-free-music-2466.mp3'
+);
 
 closeBtn.addEventListener('click', function () {
   introModal.classList.remove('bg-active');
+  bgMusic.play();
 });
 food.addEventListener('click', function () {
   location.href = nextLevel;
